a9f24dc4b290e465d10549af2ad77dda40b7268b
CarlosEduardo7x <00001098067344sp@al.educacao.sp.gov.br>
2024-11-21 17:24:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano o Corinthians foi fundado?',
+    'O Corinthians foi fundado em 1910.'
+);
+
+criaCartao(
+    'Jogadores',
+    'Quem é o maior artilheiro da história do Corinthians?',
+    'O maior artilheiro da história do Corinthians é o Lula, com 305 gols.'
+);
+
+criaCartao(
+    'Títulos',
+    'Quantos títulos da Copa do Mundo o Corinthians conquistou?',
+    'O Corinthians conquistou 2 títulos da Copa do Mundo: em 2000 e em 2012.'
+);
+
+criaCartao(
+    'Estádio',
+    'Qual é o nome do estádio do Corinthians?',
+    'O nome do estádio é Arena Corinthians, também conhecido como Arena Itaquera.'
+);
+
+criaCartao(
+    'Rivalidade',
+    'Quem é o principal rival do Corinthians?',
+    'O principal rival do Corinthians é o Palmeiras, conhecido como "Paulista Derby".'
+);
+
+criaCartao(
+    'Ídolos',
+    'Quem é o maior ídolo da história do Corinthians?',
+    'O maior ídolo do Corinthians é Sócrates, famoso pela sua habilidade e liderança.'
+);
